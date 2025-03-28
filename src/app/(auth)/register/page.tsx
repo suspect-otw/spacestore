7f95d72fe316e3ebdba0acf90071b9d9e742a2e9
suspect-otw <emirturhanpv@gmail.com>
2025-03-28 17:19:09 +0300

    login and register

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -4,14 +4,25 @@ import SignUpForm from "@/components/SignUpForm";
 
 export default function RegisterPage() {
   return (
-    <>
+    <div className="flex flex-col min-h-screen w-full">
       <Navbar />
-      <div className="flex min-h-svh flex-col items-center justify-center bg-muted p-6 md:p-10">
-        <div className="w-full max-w-sm md:max-w-3xl">
-          <SignUpForm />
+      <div className="flex-1 grid min-h-[calc(100vh-4rem)] lg:grid-cols-2 w-full">
+        <div className="flex flex-col p-6 md:p-10">
+          <div className="flex flex-1 items-center justify-center">
+            <div className="w-full max-w-md lg:max-w-lg">
+              <SignUpForm />
+            </div>
+          </div>
+        </div>
+        <div className="relative hidden lg:block">
+          <img
+            src="/register.svg"
+            alt="Register Illustration"
+            className="absolute inset-0 h-full w-full object-contain p-6"
+          />
         </div>
       </div>
       <Footer />
-    </>
+    </div>
   );
 }
